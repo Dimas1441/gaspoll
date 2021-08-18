18bc4a933a9bddf33293c93248b12cc324fc380d
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-18 22:16:46 +0700

    Update menu.sh

--- menu-all/menu.sh
+++ menu-all/menu.sh
@@ -3,17 +3,7 @@ yl='\e[031;1m'
 bl='\e[36;1m'
 gl='\e[32;1m'
 
-if [ "${EUID}" -ne 0 ]; then
-		echo "You need to run this script as root"
-		exit 1
-fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
-fi
-red='\e[1;31m'
-green='\e[0;32m'
-NC='\e[0m'
+
 MYIP=$(wget -qO- icanhazip.com);
 IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/gaspoll/main/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then

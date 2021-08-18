4dc5c3b972e6e4f4655b2295c90e384dacf84ccb
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-18 18:52:42 +0700

    Update xp.sh

--- xp.sh
+++ xp.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipv4.wildyproject.com);
-echo "Script By ssh sedang"
+echo "Script By SSH SEDANG"
 clear
 data=( `cat /var/lib/premium-script/data-user-l2tp | grep '^###' | cut -d ' ' -f 2`);
 now=`date +"%Y-%m-%d"`

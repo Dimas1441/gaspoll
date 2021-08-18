fdd728fe0ed0044a358148cb1edd2c3b0fdae838
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-18 20:12:41 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/gaspoll/main/ipvps | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${red}Permission Denied!${NC}";
+echo "Please Contact Admin"
+echo "Telegram t.me/sshsedang4"
+echo "WA 082311190332"
+rm -f setup.sh
+exit 0
+fi
 clear
 m="\033[0;1;36m"
 y="\033[0;1;37m"

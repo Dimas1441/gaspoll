6e70a2acfd7334144f2cd7a1534996fffec4ac0c
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-18 22:21:35 +0700

    Update menu.sh

--- menu-all/menu.sh
+++ menu-all/menu.sh
@@ -3,7 +3,6 @@ yl='\e[031;1m'
 bl='\e[36;1m'
 gl='\e[32;1m'
 
-
 MYIP=$(wget -qO- icanhazip.com);
 IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/gaspoll/main/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
@@ -15,6 +14,7 @@ echo "Telegram t.me/sshsedang4"
 echo "WA 082311190332"
 exit 0
 fi
+
 clear 
 cat /usr/bin/bannerku | lolcat
 echo -e ""

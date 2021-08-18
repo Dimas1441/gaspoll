3c303aa3b7bfca83e10c99822b96e42597a2cc2b
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-18 18:14:34 +0700

    Update port-vless.sh

--- port-vless.sh
+++ port-vless.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipv4.wildyproject.com);
-echo "Script By AkbarSSH"
+echo "Script By SSH SEDANG"
 clear
 tls="$(cat ~/log-install.txt | grep -w "Vless TLS" | cut -d: -f2|sed 's/ //g')"
 none="$(cat ~/log-install.txt | grep -w "Vless None TLS" | cut -d: -f2|sed 's/ //g')"

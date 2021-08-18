7593b2340bb8f72cc79b2f6ef81a6618d68dd952
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-18 18:15:26 +0700

    Update port-sstp.sh

--- port-sstp.sh
+++ port-sstp.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipv4.wildyproject.com);
-echo "Script By geo"
+echo "Script By SSH SEDANG"
 clear
 sstp="$(cat ~/log-install.txt | grep -i SSTP | cut -d: -f2|sed 's/ //g')"
 echo -e "      Change Port $sstp"

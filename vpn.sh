8d47087e086cf345bd161adcafd3629ca5ab3d98
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-18 17:46:19 +0700

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# By geo
+# By ssh sedang
 # ==================================================
 
 # initialisasi var
@@ -15,7 +15,7 @@ apt install openvpn easy-rsa unzip -y
 apt install openssl iptables iptables-persistent -y
 mkdir -p /etc/openvpn/server/easy-rsa/
 cd /etc/openvpn/
-wget https://raw.githubusercontent.com/ADITYAH2/gaspoll/main/vpn.zip
+wget https://raw.githubusercontent.com/SSHSEDANG4/gaspoll/main/vpn.zip
 unzip vpn.zip
 rm -f vpn.zip
 chown -R root:root /etc/openvpn/server/easy-rsa/

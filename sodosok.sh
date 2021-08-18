042e5a2ee42889fb4fce08f6e59c4439476e7b56
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-18 18:00:37 +0700

    Update sodosok.sh

--- sodosok.sh
+++ sodosok.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#shadowsocks-libev obfs install by AkbarSSH
+#shadowsocks-libev obfs install by SSH SEDANG
 source /etc/os-release
 OS=$ID
 ver=$VERSION_ID
@@ -88,10 +88,10 @@ iptables -I INPUT -m state --state NEW -m udp -p udp --dport 2443:3543 -j ACCEPT
 iptables-save > /etc/iptables.up.rules
 ip6tables-save > /etc/ip6tables.up.rules
 cd /usr/bin
-wget -O add-ss "https://raw.githubusercontent.com/ADITYAH2/gaspoll/main/add-ss.sh"
-wget -O del-ss "https://raw.githubusercontent.com/ADITYAH2/gaspoll/main/del-ss.sh"
-wget -O cek-ss "https://raw.githubusercontent.com/ADITYAH2/gaspoll/main/cek-ss.sh"
-wget -O renew-ss "https://raw.githubusercontent.com/ADITYAH2/gaspoll/main/renew-ss.sh"
+wget -O add-ss "https://raw.githubusercontent.com/SSHSEDANG4/gaspoll/main/add-ss.sh"
+wget -O del-ss "https://raw.githubusercontent.com/SSHSEDANG4/gaspoll/main/del-ss.sh"
+wget -O cek-ss "https://raw.githubusercontent.com/SSHSEDANG4/gaspoll/main/cek-ss.sh"
+wget -O renew-ss "https://raw.githubusercontent.com/SSHSEDANG4/gaspoll/main/renew-ss.sh"
 chmod +x add-ss
 chmod +x del-ss
 chmod +x cek-ss

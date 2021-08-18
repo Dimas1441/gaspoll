4b8a6758dfc97cea2b5c876134c7fbed5d0f5178
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-08-18 18:16:19 +0700

    Update port-squid.sh

--- port-squid.sh
+++ port-squid.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- ipv4.wildyproject.com);
-echo "Script By geo"
+echo "Script By SSH SEDANG"
 clear
 sqd="$(cat /etc/squid/squid.conf | grep -i http_port | awk '{print $2}' | head -n1)"
 sqd2="$(cat /etc/squid/squid.conf | grep -i http_port | awk '{print $2}' | tail -n1)"

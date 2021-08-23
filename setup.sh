5e930289af9347a1b0d63ead56a55b7ed40a171f
Dimas1441 <89393904+Dimas1441@users.noreply.github.com>
2021-08-24 06:03:44 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-IZIN=$( curl https://raw.githubusercontent.com/SSHSEDANG4/gaspoll/main/ipvps | grep $MYIP )
+IZIN=$( curl https://raw.githubusercontent.com/Dimas1441/gaspoll/main/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
